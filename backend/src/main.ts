14a97510e62b5ba690004ffa84539ec859854774
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-04 21:17:57 +0200

    feat(backend): rename main.js to main.ts and add logic

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+import { mainConfig } from "./configs/main.config";
+import { dataBaseService } from "./services/database.service";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const startServer = async () => {
+    try {
+        const PORT = mainConfig.PORT;
+        await dataBaseService.connectToDataBase();
+        app.listen(PORT, () => {
+            console.log(`Server is starting on the port ${PORT}`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+startServer();
