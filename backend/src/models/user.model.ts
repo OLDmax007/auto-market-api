d8773cf6edbeeceaf33bb7e87ea20d08e5d1736a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-08 13:45:02 +0200

    feat(models): create and init User model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+import { UserType } from "../types/user.type";
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        age: { type: Number, required: true },
+        password: { type: String, required: true },
+        email: { type: String, unique: true, required: true },
+        platformRoleId: {
+            type: Schema.Types.ObjectId,
+            ref: "PlatformRole",
+            required: true,
+        },
+        organizationId: {
+            type: Schema.Types.ObjectId,
+            ref: "Organization",
+            default: null,
+        },
+        isActive: { type: Boolean, default: true },
+        isVerified: { type: Boolean, default: false },
+        deletedAt: { type: Date, default: null },
+    },
+    { timestamps: true, versionKey: false },
+);
+
+export const User = model<UserType>("users", userSchema);
