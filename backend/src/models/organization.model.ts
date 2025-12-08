70ec7f58e64b3868199f4f853a8dec36b89eee04
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-08 13:45:12 +0200

    feat(models): create and init Organization model

---
+++ backend/src/models/organization.model.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+import { PlanTypeEnum } from "../enums/plan-type.enum";
+import { OrganizationType } from "../types/organization.type";
+
+const { Schema, model } = mongoose;
+const addressSchema = new Schema(
+    {
+        country: { type: String, required: true },
+        city: { type: String, required: true },
+        street: { type: String, required: true },
+    },
+    { _id: false },
+);
+
+const organizationSchema = new Schema(
+    {
+        name: { type: String, unique: true, required: true },
+        description: { type: String, default: null },
+        ownerId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            unique: true,
+            required: true,
+        },
+        planType: {
+            type: String,
+            enum: PlanTypeEnum,
+            default: PlanTypeEnum.BASIC,
+        },
+        address: { type: addressSchema, required: true },
+        isActive: { type: Boolean, default: true, required: true },
+        deletedAt: { type: Date, default: null },
+    },
+    { timestamps: true, versionKey: false },
+);
+
+export const Organization = model<OrganizationType>(
+    "organizations",
+    organizationSchema,
+);
