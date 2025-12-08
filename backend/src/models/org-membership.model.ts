19cab2f63a055e6f6aa63041d5603b412ac55548
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-08 13:46:07 +0200

    feat(models): create and init OrgMembership model

---
+++ backend/src/models/org-membership.model.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+import { OrgMembershipType } from "../types/org-membership.type";
+
+const { Schema, model } = mongoose;
+const orgMembershipSchema = new Schema(
+    {
+        organizationId: {
+            type: Schema.Types.ObjectId,
+            ref: "Organization",
+            unique: true,
+            required: true,
+        },
+
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            unique: true,
+            required: true,
+        },
+
+        orgRoleId: {
+            type: Schema.Types.ObjectId,
+            ref: "OrgRole",
+            required: true,
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+        joinedAt: {
+            type: Date,
+            default: Date.now,
+        },
+
+        invitedAt: {
+            type: Date,
+            default: null,
+        },
+
+        deletedAt: {
+            type: Date,
+            default: null,
+        },
+    },
+    { timestamps: true, versionKey: false },
+);
+
+export const OrgMembership = model<OrgMembershipType>(
+    "organization-memberships",
+    orgMembershipSchema,
+);
