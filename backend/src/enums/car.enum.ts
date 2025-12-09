905480d6e97ea512ccaca1196b843f2ef1748892
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-09 08:46:02 +0200

    feat(enums): create and init CarEnum

---
+++ backend/src/enums/car.enum.ts
@@ -0,0 +1,13 @@
+export enum EngineEnum {
+    GASOLINE = "Gasoline",
+    DIESEL = "Diesel",
+    ELECTRIC = "Electric",
+    HYBRID = "Hybrid",
+    GAS = "Gas",
+}
+
+export enum TransmissionEnum {
+    AUTOMATIC = "Automatic",
+    MANUAL = "Manual",
+    ROBOTIC = "Robotic",
+}
