1ba9f1ae5b9ac57404abc5c26a376c0a0538730f
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-12 10:53:54 +0200

    feat(types): add UserCreateDtoType and UserCreateDbType to user.type.ts

--- backend/src/types/user.type.ts
+++ backend/src/types/user.type.ts
@@ -1,3 +1,4 @@
+import { PlanTypeEnum } from "../enums/plan-type.enum";
 import { BaseType } from "./base.type";
 
 export type UserType = {
@@ -9,7 +10,16 @@ export type UserType = {
     email: string;
     platformRoleId: string;
     organizationId: string;
+    planType: PlanTypeEnum | null;
     isActive: boolean;
     isVerified: boolean;
     deletedAt: Date | null;
 } & BaseType;
+
+export type UserCreateDtoType = Pick<
+    UserType,
+    "firstName" | "lastName" | "age" | "email" | "password"
+>;
+
+export type UserCreateDbType = Pick<UserType, "platformRoleId" | "planType"> &
+    UserCreateDtoType;
