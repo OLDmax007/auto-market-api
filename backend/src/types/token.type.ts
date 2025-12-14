1238e117133f5bcb762bdf40cca600d531bf5220
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-15 01:05:49 +0200

    feat(types): create and init TokenType and TokenPayloadType

---
+++ backend/src/types/token.type.ts
@@ -0,0 +1,16 @@
+import { BaseType } from "./base.type";
+import { UserType } from "./user.type";
+
+export type TokenType = {
+    _id: string;
+    accessToken: string;
+    refreshToken: string;
+    userId: string;
+} & BaseType;
+
+export type TokenPayloadType = Pick<
+    UserType,
+    "firstName" | "lastName" | "platformRoleId" | "email"
+> & {
+    userId: string;
+};
