d471a663f8c2e7084291c2eb8fbcba8663e83d06
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-17 21:04:04 +0200

    feat(user): add "create" to UserService

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,5 +1,10 @@
+import { HttpStatusEnum } from "../enums/http-status.enum";
+import { PlanTypeEnum } from "../enums/plan-type.enum";
+import { PlatformRoleEnum } from "../enums/platform-role.enum";
+import { ApiError } from "../errors/api.error";
+import { platformRoleRepository } from "../repositories/platform-role.repository";
 import { userRepository } from "../repositories/user.repository";
-import { UserType } from "../types/user.type";
+import { UserCreateDtoType, UserType } from "../types/user.type";
 
 class UserService {
     public getAll(): Promise<UserType[]> {
@@ -8,6 +13,24 @@ class UserService {
 
     public getById(id: string): Promise<UserType> {
         return userRepository.getById(id);
+    }
+
+    public async create(dto: UserCreateDtoType): Promise<UserType> {
+        const role = await platformRoleRepository.getByName(
+            PlatformRoleEnum.VISITOR,
+        );
+
+        if (!role) {
+            throw new ApiError(HttpStatusEnum.NOT_FOUND, "Role not found");
+        }
+
+        const user = await userRepository.create({
+            platformRoleId: role._id,
+            planType: PlanTypeEnum.BASIC,
+            ...dto,
+        });
+
+        return user;
     }
 }
 
