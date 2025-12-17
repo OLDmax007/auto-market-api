bead5219664fa518bb63938d68d4ee6de709a4e3
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-12-18 01:06:48 +0200

    feat(auth): add verifyToken to TokenService

--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 
 import { mainConfig } from "../configs/main.config";
+import { HttpStatusEnum } from "../enums/http-status.enum";
+import { TokenTypeEnum } from "../enums/token-type.enum";
+import { ApiError } from "../errors/api.error";
 import { TokenPayloadType } from "../types/token.type";
 
 const {
@@ -19,6 +22,21 @@ class TokenService {
             expiresIn: JWT_REFRESH_LIFETIME,
         });
         return { accessToken, refreshToken };
+    }
+
+    public verifyToken(token: string, tokenType: TokenTypeEnum) {
+        let secret: string;
+        switch (tokenType) {
+            case TokenTypeEnum.ACCESS:
+                secret = mainConfig.JWT_ACCESS_SECRET;
+                break;
+            case TokenTypeEnum.REFRESH:
+                secret = mainConfig.JWT_REFRESH_SECRET;
+                break;
+            default:
+                throw new ApiError(HttpStatusEnum.UNAUTHORIZED, "Unauthorized");
+        }
+        return jwt.verify(token, secret);
     }
 }
 
