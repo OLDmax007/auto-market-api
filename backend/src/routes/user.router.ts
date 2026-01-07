a7a1424e985f8237b400063466b36f040484e30d
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-07 22:55:31 +0200

    feat(routes): add become seller, upgrade plan routes  to userRouter

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
+import { TokenTypeEnum } from "../enums/token-type.enum";
+import { authMiddleware } from "../middlewares/auth.middleware";
 import { commonMiddleware } from "../middlewares/common.middleware";
 
 const router = Router();
@@ -11,5 +13,19 @@ router.get(
     commonMiddleware.isValidId("userId"),
     userController.getById,
 );
+
+router.patch(
+    "/become-seller",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.becomeSeller,
+);
+
+router.patch(
+    "/upgrade-plan",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.upgradeToPremium,
+);
+
+router.post;
 
 export const userRouter = router;
