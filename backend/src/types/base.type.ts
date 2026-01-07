ca2022eeecc4155e45975b92ebad674d2901d1e4
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-07 21:29:18 +0200

    feat(types): create and add CurrencyType to BaseType

--- backend/src/types/base.type.ts
+++ backend/src/types/base.type.ts
@@ -1,4 +1,11 @@
+import { CurrencyEnum } from "../enums/currency.enum";
+
 export type BaseType = {
     createdAt: Date;
     updatedAt: Date;
+};
+
+export type CurrencyType = {
+    amount: number;
+    currency: CurrencyEnum;
 };
