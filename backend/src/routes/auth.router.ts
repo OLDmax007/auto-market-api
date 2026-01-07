156c72f069f1c2de92a7dc7606978e596f7c3256
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-07 22:54:48 +0200

    feat(routes): add refresh route to authRouter

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 
 import { authController } from "../controllers/auth.controller";
+import { TokenTypeEnum } from "../enums/token-type.enum";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/sign-up", authController.signUp);
 router.post("/sign-in", authController.signIn);
+
+router.post(
+    "/refresh",
+    authMiddleware.checkToken(TokenTypeEnum.REFRESH),
+    authController.refresh,
+);
 
 export const authRouter = router;
