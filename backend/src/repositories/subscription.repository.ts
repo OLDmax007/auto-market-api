4fe0561a7356d26e62b59da7f06c5c243071f080
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-07 22:17:23 +0200

    feat(billing): create and implement SubscriptionRepository

---
+++ backend/src/repositories/subscription.repository.ts
@@ -0,0 +1,24 @@
+import { Subscription } from "../models/billing/subscription.model";
+import {
+    SubscriptionCreateType,
+    SubscriptionType,
+} from "../types/billing/subcription.type";
+
+class SubscriptionRepository {
+    public getById(id: string): Promise<SubscriptionType> {
+        return Subscription.findById(id);
+    }
+
+    public create(dto: SubscriptionCreateType): Promise<SubscriptionType> {
+        return Subscription.create(dto);
+    }
+
+    public updateById(
+        id: string,
+        dto: Omit<SubscriptionCreateType, "userId">,
+    ): Promise<SubscriptionType> {
+        return Subscription.findByIdAndUpdate(id, dto, { new: true });
+    }
+}
+
+export const subscriptionRepository = new SubscriptionRepository();
