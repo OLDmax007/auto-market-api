c675fa5306e546c9ec0f4d41d7a1d33d8df86396
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-07 21:30:44 +0200

    feat(types): update SubscriptionType and add SubscriptionCreateType

--- backend/src/types/billing/subcription.type.ts
+++ backend/src/types/billing/subcription.type.ts
@@ -1,11 +1,17 @@
 import { PlanTypeEnum } from "../../enums/plan-type.enum";
-import { BaseType } from "../base.type";
+import { BaseType, CurrencyType } from "../base.type";
 
 export type SubscriptionType = {
     _id: string;
     userId: string;
     planType: PlanTypeEnum;
+    price: CurrencyType;
     activeFrom: Date;
     activeTo: Date | null;
     isActive: boolean;
 } & BaseType;
+
+export type SubscriptionCreateType = Omit<
+    SubscriptionType,
+    "createdAt" | "updatedAt" | "_id"
+>;
