e684733a23506087d040c3c36c416dd63d4fa28a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-12 00:26:36 +0200

    refactor(types): delete CurrencyAmountType

--- backend/src/types/base.type.ts
+++ backend/src/types/base.type.ts
@@ -1,11 +1,4 @@
-import { CurrencyEnum } from "../enums/currency.enum";
-
 export type BaseType = {
     createdAt: Date;
     updatedAt: Date;
-};
-
-export type CurrencyAmountType = {
-    amount: number;
-    currency: CurrencyEnum;
 };
