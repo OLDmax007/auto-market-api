b93be1dce77456b6fc0c755776921f029e8c538d
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-13 00:25:13 +0200

    feat(backend): add cronRunner

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 
 import { mainConfig } from "./configs/main.config";
+import { cronRunner } from "./crons";
 import { ApiError } from "./errors/api.error";
 import { apiRouter } from "./routes/api.router";
 import { dataBaseService } from "./services/database.service";
@@ -22,6 +23,7 @@ const startServer = async () => {
         const PORT = mainConfig.PORT;
         await dataBaseService.connectToDB();
         app.listen(PORT, () => {
+            cronRunner();
             console.log(`Server is starting on the port ${PORT}`);
         });
     } catch (e) {
