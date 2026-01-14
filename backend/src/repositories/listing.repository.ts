7221c22b0c0031135db4a476f45d38a3491e8612
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-14 23:32:36 +0200

    feat(listing): create and implement Listing Repository with basic CRUD operations

---
+++ backend/src/repositories/listing.repository.ts
@@ -0,0 +1,29 @@
+import { Listing } from "../models/listing/listing.model";
+import { ListingCreateDbType, ListingType } from "../types/listing.type";
+
+class ListingRepository {
+    public getAll(): Promise<ListingType[]> {
+        return Listing.find();
+    }
+
+    public getById(id: string): Promise<ListingType> {
+        return Listing.findById(id);
+    }
+
+    public create(dto: ListingCreateDbType): Promise<ListingType> {
+        return Listing.create(dto);
+    }
+
+    public updateById(
+        id: string,
+        dto: Partial<ListingCreateDbType>,
+    ): Promise<ListingType> {
+        return Listing.findByIdAndUpdate(id, dto, { new: true });
+    }
+
+    public deleteById(id: string): Promise<ListingType> {
+        return Listing.findByIdAndDelete(id);
+    }
+}
+
+export const listingRepository = new ListingRepository();
