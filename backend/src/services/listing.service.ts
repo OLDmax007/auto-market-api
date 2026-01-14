f0728501360d83d1a0f3fcdf42b86ed7dc1e8d89
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-14 23:44:56 +0200

    feat(listing): add create to ListingService

--- backend/src/services/listing.service.ts
+++ backend/src/services/listing.service.ts
@@ -1,5 +1,9 @@
+import { HttpStatusEnum } from "../enums/http-status.enum";
+import { ApiError } from "../errors/api.error";
 import { listingRepository } from "../repositories/listing.repository";
-import { ListingType } from "../types/listing.type";
+import { ListingCreateDtoType, ListingType } from "../types/listing.type";
+import { pricingService } from "./pricing.service";
+import { userService } from "./user.service";
 
 class ListingService {
     public getAll(): Promise<ListingType[]> {
@@ -8,6 +12,32 @@ class ListingService {
 
     public getById(id: string): Promise<ListingType> {
         return listingRepository.getById(id);
+    }
+    public async create(
+        userId: string,
+        { enteredPrice: { amount, currency }, ...newDto }: ListingCreateDtoType,
+    ): Promise<ListingType> {
+        const { organizationId } = await userService.getById(userId);
+
+        const prices = await pricingService.calculateListingPrices(
+            amount,
+            currency,
+        );
+
+        if (!prices.length) {
+            throw new ApiError(
+                HttpStatusEnum.BAD_REQUEST,
+                "PRICING_CALCULATION_FAILED",
+            );
+        }
+
+        return listingRepository.create({
+            userId,
+            organizationId,
+            prices,
+            publishedAt: new Date(),
+            ...newDto,
+        });
     }
 }
 
