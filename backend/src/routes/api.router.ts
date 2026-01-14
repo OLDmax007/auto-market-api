5af14f41f06b3cdaddb1e62530cbb3890cce4bbc
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-14 23:49:36 +0200

    feat(routes): add listingRouter to apiRouter

--- backend/src/routes/api.router.ts
+++ backend/src/routes/api.router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { authRouter } from "./auth.router";
+import { listingRouter } from "./listing.router";
 import { userRouter } from "./user.router";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
+router.use("/listings", listingRouter);
 
 export const apiRouter = router;
