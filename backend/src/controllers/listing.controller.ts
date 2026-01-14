f8f1089d3533e886d1fe60e90631410425fd5102
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-14 23:47:54 +0200

    feat(listing): add create to ListingController

--- backend/src/controllers/listing.controller.ts
+++ backend/src/controllers/listing.controller.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 import { HttpStatusEnum } from "../enums/http-status.enum";
 import { listingService } from "../services/listing.service";
+import { ListingCreateDtoType } from "../types/listing.type";
+import { TokenPayloadType } from "../types/token.type";
 
 class ListingController {
     public async getAll(req: Request, res: Response, next: NextFunction) {
@@ -18,6 +20,17 @@ class ListingController {
             const { listingId } = req.params as { listingId: string };
             const data = await listingService.getById(listingId);
             res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { userId } = req.res.locals.payload as TokenPayloadType;
+            const dto = req.body as ListingCreateDtoType;
+            const data = await listingService.create(userId, dto);
+            res.status(HttpStatusEnum.CREATED).json(data);
         } catch (e: unknown) {
             next(e);
         }
