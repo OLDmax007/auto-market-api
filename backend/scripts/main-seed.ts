a875f009515e9c3697bad876b030c1c2c808fb26
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-25 17:59:55 +0200

    feat(seeds): add updateRates to mainSeed

--- backend/scripts/main-seed.ts
+++ backend/scripts/main-seed.ts
@@ -1,10 +1,12 @@
 import {dataBaseService} from "../src/services/database.service";
 import {seedPermissionsAndRoles} from "./seed-permissions-and-roles";
 import {seedLocations} from "./seed-locations";
+import {pricingService} from "../src/services/pricing.service";
 
 const mainSeed = async () => {
     try {
         await dataBaseService.connectToDB();
+        await pricingService.updateRates()
         await seedPermissionsAndRoles();
         await seedLocations();
         await dataBaseService.disconnectDB();
