95dfb58948625b61ccf48fac3a0d9d85f23f7a53
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-25 18:03:18 +0200

    feat(types): add listing types

--- backend/src/types/listing-statics.type.ts
+++ backend/src/types/listing-statics.type.ts
@@ -10,3 +10,27 @@ export type ListingStaticsType = {
         month: number;
     };
 } & BaseType;
+
+export type ListingStaticsCreateDtoType = Pick<
+    ListingStaticsType,
+    "views" | "listingId"
+>;
+
+type ListingAveragePriceType = {
+    averagePrice: number;
+    listingCount: number;
+};
+
+export type ListingAveragePriceByLocationType = {
+    city: ListingAveragePriceType;
+    region: ListingAveragePriceType;
+    country: ListingAveragePriceType;
+};
+
+export type MarketAnalyticsDtoType = {
+    make: string;
+    model: string;
+    city: string;
+    region: string;
+    country: string;
+};
