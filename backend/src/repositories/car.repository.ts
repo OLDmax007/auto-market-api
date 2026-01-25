60212b2aca43e3085255a99ef1da491e36883e3b
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-26 00:32:37 +0200

    feat(car): create and implement CarRepository

---
+++ backend/src/repositories/car.repository.ts
@@ -0,0 +1,15 @@
+import { CarMarkEnum } from "../enums/car.enum";
+import { Car } from "../models/car.model";
+import { CarMapType } from "../types/car.type";
+
+class CarRepository {
+    public async getAll(): Promise<CarMapType[]> {
+        return Car.findOne();
+    }
+
+    public async getByMake(make: CarMarkEnum): Promise<CarMapType> {
+        return Car.findOne({ make });
+    }
+}
+
+export const carRepository = new CarRepository();
