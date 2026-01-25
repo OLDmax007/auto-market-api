5fe7307832c9051087f8f689860f3f18a7c6e8a4
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-25 18:26:11 +0200

    feat(listing-statics): create and implement ListingStaticsRepository

---
+++ backend/src/repositories/listing-static.repository.ts
@@ -0,0 +1,41 @@
+import { PeriodEnum } from "../enums/period.enum";
+import { ListingStatics } from "../models/listing/listing-statics.model";
+import {
+    ListingStaticsCreateDtoType,
+    ListingStaticsType,
+} from "../types/listing-statics.type";
+
+class ListingStaticsRepository {
+    public getViewsByListingId(listingId: string): Promise<ListingStaticsType> {
+        return ListingStatics.findOne({ listingId });
+    }
+
+    public createViews(dto: ListingStaticsCreateDtoType) {
+        return ListingStatics.create(dto);
+    }
+
+    public incrementViewsByListingId(listingId: string) {
+        return ListingStatics.findOneAndUpdate(
+            { listingId },
+            {
+                $inc: {
+                    "views.all": 1,
+                    "views.day": 1,
+                    "views.week": 1,
+                    "views.month": 1,
+                },
+            },
+            { new: true, projection: { views: 1, listingId: 1 } },
+        );
+    }
+
+    public resetViews(period: PeriodEnum) {
+        console.log(period);
+        return ListingStatics.updateMany(
+            {},
+            { $set: { [`views.${period}`]: 50 } },
+        );
+    }
+}
+
+export const listingStaticRepository = new ListingStaticsRepository();
