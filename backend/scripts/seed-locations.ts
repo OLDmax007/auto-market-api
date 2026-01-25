956697a8e40db3a8a169b918ab0200bf609efa62
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-26 00:27:06 +0200

    feat(seeds): update seedLocations

--- backend/scripts/seed-locations.ts
+++ backend/scripts/seed-locations.ts
@@ -1,10 +1,10 @@
-import {LocationModel} from "../src/models/location.model";
 import {LocationMap} from "../src/configs/location-map.config";
+import {Location} from "../src/models/location.model";
 
 export const seedLocations = async () => {
     try {
-        await LocationModel.deleteMany({});
-        await LocationModel.insertMany(LocationMap);
+        await Location.deleteMany({});
+        await Location.insertMany(LocationMap);
         console.log("Locations seeded successfully");
     } catch (err) {
         console.error("Error seeding locations:", err);
