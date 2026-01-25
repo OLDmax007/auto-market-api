bd99c8ecb24793ca66da51af2fb2672285cf783c
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-01-26 00:28:14 +0200

    feat(crons): add reset crons to cronRunner

--- backend/src/crons/index.ts
+++ backend/src/crons/index.ts
@@ -1,5 +1,13 @@
+import {
+    resetDayViewsCron,
+    resetMonthViewsCron,
+    resetWeekViewsCron,
+} from "./reset-views.cron";
 import { updateRatesCron } from "./update-rates.cron";
 
 export const cronRunner = () => {
     updateRatesCron.start();
+    resetDayViewsCron.start();
+    resetWeekViewsCron.start();
+    resetMonthViewsCron.start();
 };
