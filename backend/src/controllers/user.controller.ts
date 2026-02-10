2141515ca3547772169f3d46f96b1a09f5dbfd98
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-10 22:35:39 +0200

    feat(user): implement full user management and account actions in UserController

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,6 +4,10 @@ import { HttpStatusEnum } from "../enums/http-status.enum";
 import { userService } from "../services/user.service";
 import { CurrencyAmountType } from "../types/rate.type";
 import { TokenPayloadType } from "../types/token.type";
+import {
+    UserUpdateByAdminDtoType,
+    UserUpdateDtoType,
+} from "../types/user.type";
 
 class UserController {
     public async getAll(req: Request, res: Response, next: NextFunction) {
@@ -25,9 +29,80 @@ class UserController {
         }
     }
 
-    public async becomeSeller(req: Request, res: Response, next: NextFunction) {
+    public async getMe(req: Request, res: Response, next: NextFunction) {
         try {
             const { userId } = req.res.locals.payload as TokenPayloadType;
+            const data = await userService.getById(userId);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async updateMe(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { userId } = req.res.locals.payload as TokenPayloadType;
+            const dto = req.body as UserUpdateDtoType;
+            const data = await userService.updateMe(userId, dto);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async updateByAdmin(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) {
+        try {
+            const { userId } = req.params as { userId: string };
+            const dto = req.body as UserUpdateByAdminDtoType;
+            const data = await userService.updateByAdmin(userId, dto);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async deleteById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { userId } = req.params as { userId: string };
+            const data = await userService.deleteById(userId);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async activateUser(req: Request, res: Response, next: NextFunction) {
+        try {
+            console.log("dog");
+            const { userId } = req.params as { userId: string };
+            const data = await userService.activateUser(userId);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async deactivateUser(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) {
+        try {
+            const { userId } = req.params as { userId: string };
+            const data = await userService.deactivateUser(userId);
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async becomeSeller(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { userId } = res.locals.payload as TokenPayloadType;
             const data = await userService.becomeSeller(userId);
             res.status(HttpStatusEnum.OK).json(data);
         } catch (e: unknown) {
@@ -41,7 +116,7 @@ class UserController {
         next: NextFunction,
     ) {
         try {
-            const { userId } = req.res.locals.payload as TokenPayloadType;
+            const { userId } = res.locals.payload as TokenPayloadType;
             const data = await userService.upgradeToPremium(userId);
             res.status(HttpStatusEnum.OK).json(data);
         } catch (e: unknown) {
@@ -51,7 +126,7 @@ class UserController {
 
     public async topUpBalance(req: Request, res: Response, next: NextFunction) {
         try {
-            const { userId } = req.res.locals.payload as TokenPayloadType;
+            const { userId } = res.locals.payload as TokenPayloadType;
             const dto = req.body as CurrencyAmountType;
             const data = await userService.topUpBalance(userId, dto);
             res.status(HttpStatusEnum.OK).json(data);
