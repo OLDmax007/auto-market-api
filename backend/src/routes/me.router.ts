b8446021f53cbcd62ebf3fd67171e8f3fad4ffbe
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-10 22:37:40 +0200

    feat(routes): create and implement meRouter with routes

---
+++ backend/src/routes/me.router.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+import { userController } from "../controllers/user.controller";
+import { TokenTypeEnum } from "../enums/token-type.enum";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router.get(
+    "/me",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.getMe,
+);
+router.patch(
+    "/me",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.updateMe,
+);
+router.patch(
+    "/become-seller",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.becomeSeller,
+);
+router.patch(
+    "/upgrade-plan",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.upgradeToPremium,
+);
+router.patch(
+    "/top-up-balance",
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userController.topUpBalance,
+);
+
+export const meRouter = router;
