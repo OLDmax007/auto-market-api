1188ccc63e6cdd2f186e009a022421801d651e2d
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-13 01:08:42 +0200

    chore(config): add action tokens and email secrets to mainConfig

--- backend/src/configs/main.config.ts
+++ backend/src/configs/main.config.ts
@@ -5,6 +5,8 @@ import { config } from "dotenv";
 config({ path: path.resolve(__dirname, "../../../.env") });
 
 type MainConfig = {
+    FRONTEND_URL: String;
+    HOST: string;
     PORT: string;
     MONGO_URI: string;
     DB_MAX_RETIRES: number;
@@ -14,9 +16,15 @@ type MainConfig = {
     JWT_REFRESH_LIFETIME: any;
     EMAIL_USER: string;
     EMAIL_PASSWORD: string;
+    JWT_VERIFY_SECRET: string;
+    JWT_VERIFY_LIFETIME: string;
+    JWT_RECOVERY_SECRET: string;
+    JWT_RECOVERY_LIFETIME: string;
 };
 
 export const mainConfig: MainConfig = {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    HOST: process.env.HOST,
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     DB_MAX_RETIRES: Number(process.env.DB_MAX_RETIRES),
@@ -26,4 +34,8 @@ export const mainConfig: MainConfig = {
     JWT_REFRESH_LIFETIME: process.env.JWT_REFRESH_LIFETIME,
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    JWT_VERIFY_SECRET: process.env.JWT_VERIFY_SECRET,
+    JWT_VERIFY_LIFETIME: process.env.JWT_VERIFY_LIFETIME,
+    JWT_RECOVERY_SECRET: process.env.JWT_RECOVERY_SECRET,
+    JWT_RECOVERY_LIFETIME: process.env.JWT_RECOVERY_LIFETIME,
 };
