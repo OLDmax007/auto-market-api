f69db19c8d6d7e784746aabbbbbf9d771a64c548
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-13 23:27:33 +0200

    fix(helpers): add checking for returning the path with the token

--- backend/src/helpers/link-builder.helper.ts
+++ backend/src/helpers/link-builder.helper.ts
@@ -1,6 +1,9 @@
-export const buildLink = (path: string, token: string): string => {
-    const frontendUrl = process.env.FRONTEND_URL;
-    if (!frontendUrl) throw new Error("FRONTEND_URL is not defined in .env");
+import { mainConfig } from "../configs/main.config";
 
-    return `${frontendUrl}${path}?token=${token}`;
+export const buildLink = (path: string, token?: string): string => {
+    const frontendUrl = mainConfig.FRONTEND_URL;
+    if (!frontendUrl) throw new Error("FRONTEND_URL is not defined in .env");
+    return token
+        ? `${frontendUrl}${path}?token=${token}`
+        : `${frontendUrl}${path}`;
 };
