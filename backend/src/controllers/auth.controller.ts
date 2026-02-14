e1f683f99714344dc3766d26ea3923a124549e2a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-14 21:54:17 +0200

    feat(auth): add reset password methods to AuthController

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
+import { ActionTokenEnum } from "../enums/action-token.enum";
+import { EmailEnum } from "../enums/email.enum";
 import { HttpStatusEnum } from "../enums/http-status.enum";
 import { authService } from "../services/auth.service";
 import { TokenPayloadType } from "../types/token.type";
@@ -52,7 +54,44 @@ class AuthController {
     ) {
         try {
             const { email } = req.body as { email: string };
-            const data = await authService.requestEmailVerification(email);
+            const data = await authService.sendAuthActionEmail(email, {
+                emailType: EmailEnum.VERIFY_USER,
+                tokenType: ActionTokenEnum.VERIFY,
+                path: "/verify",
+            });
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async requestRecoverPassword(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) {
+        try {
+            const { email } = req.body as { email: string };
+            const data = await authService.sendAuthActionEmail(email, {
+                emailType: EmailEnum.RECOVER_PASSWORD,
+                tokenType: ActionTokenEnum.RECOVER,
+                path: "/reset-password",
+            });
+            res.status(HttpStatusEnum.OK).json(data);
+        } catch (e: unknown) {
+            next(e);
+        }
+    }
+
+    public async resetPassword(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) {
+        try {
+            const { userId } = res.locals.payload as TokenPayloadType;
+            const { password } = req.body as { password: string };
+            const data = await authService.resetPassword(userId, password);
             res.status(HttpStatusEnum.OK).json(data);
         } catch (e: unknown) {
             next(e);
