2ebc75975644ecce86b26848e40058d13a41f80f
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-15 17:03:21 +0200

    feat(types): change permissions field in PlatformRoleType and OrgRoleType

--- backend/src/types/permissions/org-role.type.ts
+++ backend/src/types/permissions/org-role.type.ts
@@ -1,9 +1,10 @@
+import { OrgPermissionEnum } from "../../enums/org-permission.enum";
 import { OrgRoleEnum } from "../../enums/org-role.enum";
 import { BaseType } from "../base.type";
 
 export type OrgRoleType = {
     _id: string;
     role: OrgRoleEnum;
-    permissionIds: string[];
+    permissions: OrgPermissionEnum[];
     description?: string;
 } & BaseType;
--- backend/src/types/permissions/platform-role.type.ts
+++ backend/src/types/permissions/platform-role.type.ts
@@ -1,9 +1,10 @@
+import { PlatformPermissionEnum } from "../../enums/platform-permission.enum";
 import { PlatformRoleEnum } from "../../enums/platform-role.enum";
 import { BaseType } from "../base.type";
 
 export type PlatformRoleType = {
     _id: string;
     role: PlatformRoleEnum;
-    permissionIds: string[];
+    permissions: PlatformPermissionEnum[];
     description?: string;
 } & BaseType;
