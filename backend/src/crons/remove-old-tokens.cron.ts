8fbaaa789e8ed84678ca20b65a41417d0d0f1b36
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-18 01:23:11 +0200

    refactor(crons): change token cleanup to run monthly in removeOldTokensCron

--- backend/src/crons/remove-old-tokens.cron.ts
+++ backend/src/crons/remove-old-tokens.cron.ts
@@ -4,14 +4,11 @@ import { mainConfig } from "../configs/main.config";
 import { timeHelper } from "../helpers/time.helper";
 import { tokenRepository } from "../repositories/token.repository";
 
-export const removeOldTokensCron = new CronJob("*/10 * * * * *", async () => {
+export const removeOldTokensCron = new CronJob("0 0 0 1 * *", async () => {
     try {
         const timeLife = mainConfig.JWT_REFRESH_LIFETIME;
         const { value, unit } = timeHelper.parseLifeTime(timeLife);
-        console.log(value, unit);
         const date = timeHelper.subFromCurrentTime(value, unit);
-
-        console.log(date);
         const count = await tokenRepository.deleteBeforeDate(date);
 
         if (count) console.log(`deleted - ${count}`);
