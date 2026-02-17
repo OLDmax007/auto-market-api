8d0d3087191515543bc2605d6675ae67703cef10
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-18 01:26:11 +0200

    feat(car): make car endpoints public in carRouter

--- backend/src/routes/car.router.ts
+++ backend/src/routes/car.router.ts
@@ -1,20 +1,10 @@
 import { Router } from "express";
 
 import { carController } from "../controllers/car.controller";
-import { TokenTypeEnum } from "../enums/token-type.enum";
-import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get(
-    "/:make/models",
-    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
-    carController.getModelsByMake,
-);
-router.get(
-    "/makes",
-    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
-    carController.getAllMakes,
-);
+router.get("/:make/models", carController.getModelsByMake);
+router.get("/makes", carController.getAllMakes);
 
 export const carRouter = router;
