fd66f35b629f5cf5b328a850d73598853132e25a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-18 01:31:05 +0200

    fix(moderation): prevent sending emails when no managers exist in ModerationService

--- backend/src/services/moderation/moderation.service.ts
+++ backend/src/services/moderation/moderation.service.ts
@@ -1,33 +1,39 @@
 import { emailConstants } from "../../constants/email-data";
 import { PlatformRoleEnum } from "../../enums/platform-role.enum";
+import { userRepository } from "../../repositories/user.repository";
 import {
     ListingModerationResultType,
     ListingType,
 } from "../../types/listing.type";
 import { emailService } from "../email.service";
 import { platformRoleService } from "../platform-role.service";
-import { userService } from "../user.service";
 
 class ModerationService {
     public async sendListWithInactiveListingToModeration(
         listing: Pick<ListingType, "_id" | "userId" | "title">,
         moderation: ListingModerationResultType,
     ): Promise<void> {
-        const { _id } = await platformRoleService.getPlatformRole(
-            PlatformRoleEnum.MANAGER,
-        );
+        const { _id: managerRoleId } =
+            await platformRoleService.getPlatformRole(PlatformRoleEnum.MANAGER);
 
-        const managers = await userService.getManyByPlatformId(_id);
+        const managers =
+            await userRepository.getManyByPlatformId(managerRoleId);
 
-        await Promise.allSettled(
-            managers.map((m) => {
-                emailService.sendEmail(m.email, emailConstants.LISTING_CHECK, {
-                    listing,
-                    moderation,
-                    managerName: `${m.firstName} ${m.lastName}`,
-                });
-            }),
-        );
+        if (managers.length >= 1) {
+            await Promise.allSettled(
+                managers.map((m) => {
+                    emailService.sendEmail(
+                        m.email,
+                        emailConstants.LISTING_CHECK,
+                        {
+                            listing,
+                            moderation,
+                            managerName: `${m.firstName} ${m.lastName}`,
+                        },
+                    );
+                }),
+            );
+        }
     }
 }
 
