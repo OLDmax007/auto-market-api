ad08481ddafeb79999f46edd94ba9c46818bbd1e
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-22 20:06:23 +0200

    refactor(models/types/hbs): delete unused files

--- backend/src/types/permissions/permission.type.ts
+++
@@ -1,9 +0,0 @@
-import { OrgPermissionEnum } from "../../enums/org-permission.enum";
-import { PlatformPermissionEnum } from "../../enums/platform-permission.enum";
-import { BaseType } from "../base.type";
-
-export type PermissionType = {
-    _id: string;
-    name: PlatformPermissionEnum | OrgPermissionEnum;
-    description?: string;
-} & BaseType;
