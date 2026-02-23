081f84192ee8542e3cd6b4bd27fae3e967f7e1a5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-23 17:03:54 +0200

    feat(profanity): add checkProfanity to ProfanityService

--- backend/src/services/profanity.service.ts
+++ backend/src/services/profanity.service.ts
@@ -3,6 +3,8 @@ import profanity from "leo-profanity";
 import { EnProfanityList } from "../constants/en-profanity-list";
 import { RuProfanityList } from "../constants/ru-profanity-list";
 import { UaProfanityList } from "../constants/ua-profanity-list";
+import { HttpStatusEnum } from "../enums/http-status.enum";
+import { ApiError } from "../errors/api.error";
 
 class ProfanityService {
     constructor() {
@@ -26,6 +28,19 @@ class ProfanityService {
 
             return words.some((word) => this.hasProfanity(word));
         });
+    }
+
+    public checkProfanity(...texts: string[]): boolean {
+        const isProfanity = this.hasAnyProfanity(...texts);
+
+        if (isProfanity) {
+            throw new ApiError(
+                HttpStatusEnum.BAD_REQUEST,
+                "Profanity detected. Please clean up your title or description",
+            );
+        }
+
+        return isProfanity;
     }
 }
 
