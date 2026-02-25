42e93010842c7e2dbd0fd4618c479f1dac1b0695
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-25 20:35:31 +0200

    feat(subscription): add StatusByUserId and setSubscriptionPlanByUserId to SubscriptionService

--- backend/src/services/subscription.service.ts
+++ backend/src/services/subscription.service.ts
@@ -2,7 +2,9 @@ import { CurrencyEnum } from "../enums/currency.enum";
 import { HttpStatusEnum } from "../enums/http-status.enum";
 import { PaymentStatusEnum } from "../enums/payment-status.enum";
 import { PlanTypeEnum } from "../enums/plan-type.enum";
+import { PlatformRoleEnum } from "../enums/platform-role.enum";
 import { ApiError } from "../errors/api.error";
+import { ensureIsStatusSame } from "../helpers/ensure.helper";
 import { paymentRepository } from "../repositories/payment.repository";
 import { subscriptionRepository } from "../repositories/subscription.repository";
 import { userRepository } from "../repositories/user.repository";
@@ -10,7 +12,9 @@ import {
     SubscriptionCreateType,
     SubscriptionType,
 } from "../types/billing/subcription.type";
+import { platformRoleService } from "./platform-role.service";
 import { userService } from "./user.service";
+import { userAccessService } from "./user-access.service";
 
 class SubscriptionService {
     public async getById(id: string): Promise<SubscriptionType> {
@@ -74,6 +78,55 @@ class SubscriptionService {
             activeFrom: new Date(),
             activeTo: null,
             planType: PlanTypeEnum.PREMIUM,
+            isActive: true,
+        });
+    }
+
+    public async setStatusByUserId(
+        userId: string,
+        initiatorId: string,
+        initiatorRole: PlatformRoleEnum,
+        isActive: boolean,
+    ): Promise<SubscriptionType> {
+        const user = await userService.getById(userId);
+
+        if (!userAccessService.isSelfAction(userId, initiatorId)) {
+            const { role } = await platformRoleService.getPlatformRoleById(
+                user.platformRoleId,
+            );
+            userAccessService.checkIsStaff(role, initiatorRole);
+        }
+        const subscription = await this.getById(user.subscriptionId);
+        ensureIsStatusSame(
+            subscription.isActive,
+            isActive,
+            `Subscription is already ${isActive ? "activated" : "deactivated"}`,
+        );
+
+        return subscriptionRepository.updateById(subscription._id, {
+            isActive,
+        });
+    }
+
+    public async setSubscriptionPlanByUserId(
+        userId: string,
+        initiatorId: string,
+        initiatorRole: PlatformRoleEnum,
+        dto: { newPlan: PlanTypeEnum },
+    ): Promise<SubscriptionType> {
+        const user = await userService.getById(userId);
+        const subscription = await this.getById(user.subscriptionId);
+
+        if (!userAccessService.isSelfAction(userId, initiatorId)) {
+            const { role } = await platformRoleService.getPlatformRoleById(
+                user.platformRoleId,
+            );
+            userAccessService.checkIsStaff(role, initiatorRole);
+        }
+        return subscriptionRepository.updateById(subscription._id, {
+            activeFrom: new Date(),
+            activeTo: null,
+            planType: dto.newPlan,
             isActive: true,
         });
     }
