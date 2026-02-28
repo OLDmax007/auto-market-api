c31f2f3ab08230b22e8c07be751fe3eda04bdfd3
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-02-28 23:04:12 +0200

    feat(routes): add listing upload and delete poster routes to listingRouter

--- backend/src/routes/listing.router.ts
+++ backend/src/routes/listing.router.ts
@@ -5,6 +5,7 @@ import { PlatformPermissionEnum } from "../enums/platform-permission.enum";
 import { TokenTypeEnum } from "../enums/token-type.enum";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { commonMiddleware } from "../middlewares/common.middleware";
+import { fileMiddleware } from "../middlewares/file.middleware";
 import { roleMiddleware } from "../middlewares/role.middleware";
 import { userMiddleware } from "../middlewares/user.middleware";
 
@@ -24,9 +25,7 @@ router.get(
     authMiddleware.checkToken(TokenTypeEnum.ACCESS),
     userMiddleware.isActiveUser,
     userMiddleware.isVerifiedUser,
-    roleMiddleware.checkPermission(
-        PlatformPermissionEnum.LISTING_GET_BY_MODERATION,
-    ),
+    roleMiddleware.checkPermission(PlatformPermissionEnum.LISTING_GET_MY_BY_ID),
     listingController.getMyById,
 );
 
@@ -96,7 +95,7 @@ router.patch(
     userMiddleware.isActiveUser,
     userMiddleware.isVerifiedUser,
     roleMiddleware.checkPermission(PlatformPermissionEnum.LISTING_ACTIVATE),
-    listingController.activateListing,
+    listingController.activate,
 );
 
 router.patch(
@@ -106,7 +105,7 @@ router.patch(
     userMiddleware.isActiveUser,
     userMiddleware.isVerifiedUser,
     roleMiddleware.checkPermission(PlatformPermissionEnum.LISTING_DEACTIVATE),
-    listingController.deactivateListing,
+    listingController.deactivate,
 );
 router.patch(
     "/:listingId/close",
@@ -117,6 +116,30 @@ router.patch(
         PlatformPermissionEnum.ME_LISTING_DEACTIVATE,
     ),
     listingController.closeMyListing,
+);
+
+router.post(
+    "/:listingId/poster",
+    commonMiddleware.isValidId("listingId"),
+    fileMiddleware.isValidFile("poster"),
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userMiddleware.isActiveUser,
+    userMiddleware.isVerifiedUser,
+    roleMiddleware.checkPermission(
+        PlatformPermissionEnum.LISTING_UPLOAD_POSTER,
+    ),
+    listingController.uploadPoster,
+);
+router.delete(
+    "/:listingId/poster",
+    commonMiddleware.isValidId("listingId"),
+    authMiddleware.checkToken(TokenTypeEnum.ACCESS),
+    userMiddleware.isActiveUser,
+    userMiddleware.isVerifiedUser,
+    roleMiddleware.checkPermission(
+        PlatformPermissionEnum.LISTING_DELETE_POSTER,
+    ),
+    listingController.deletePoster,
 );
 
 router.delete(
