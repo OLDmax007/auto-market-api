25e61907353b32550d745f607c7c8d87392af594
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-02 23:53:33 +0200

    refactor(car): rename Mark to Make and enhance missing model reporting in CarService

--- backend/src/modules/car/car.service.ts
+++ backend/src/modules/car/car.service.ts
@@ -3,14 +3,14 @@ import { EMAIL_DATA } from "../../common/constants/email-data.constants";
 import { HttpStatusEnum } from "../../common/enums/http-status.enum";
 import { ApiError } from "../../common/errors/api.error";
 import { emailService } from "../../common/services/email.service";
-import { CarMarkEnum } from "./car.enum";
+import { CarMakeEnum } from "./car.enum";
+import { CarMap } from "./car.map";
 import { carRepository } from "./car.repository";
 import { CarMapType } from "./car.type";
 
 class CarService {
-    public async getModelsByMake(make: CarMarkEnum): Promise<CarMapType> {
+    public async getModelsByMake(make: CarMakeEnum): Promise<CarMapType> {
         const cars = await carRepository.getByMake(make);
-
         if (!cars) {
             throw new ApiError(
                 HttpStatusEnum.BAD_REQUEST,
@@ -20,7 +20,7 @@ class CarService {
         return { make, models: cars.models };
     }
 
-    public async getAllMakes(): Promise<{ makes: CarMarkEnum[] }> {
+    public async getAllMakes(): Promise<{ makes: CarMakeEnum[] }> {
         const cars = await carRepository.getAll();
         const makes = cars.map((c) => c.make);
         if (!makes.length) {
@@ -30,7 +30,7 @@ class CarService {
     }
 
     public async validateCarModel(
-        make: CarMarkEnum,
+        make: CarMakeEnum,
         model: string,
     ): Promise<void> {
         const { models } = await this.getModelsByMake(make);
@@ -42,16 +42,41 @@ class CarService {
         }
     }
 
-    public async sendMissingModel(dto: { model: string }): Promise<void> {
-        if (!dto.model) {
+    public async sendMissingModel(dto: {
+        make: CarMakeEnum;
+        model: string;
+    }): Promise<void> {
+        const { make, model } = dto;
+
+        if (!model || !make) {
             throw new ApiError(
                 HttpStatusEnum.BAD_REQUEST,
-                "Provide at least make or model",
+                "Enter make and model",
             );
         }
+
+        const carFromMap = CarMap[make];
+
+        if (!carFromMap) {
+            throw new ApiError(
+                HttpStatusEnum.BAD_REQUEST,
+                `Car make '${make}' is not supported`,
+            );
+        }
+
+        const isModelExists = carFromMap.includes(dto.model);
+
+        if (isModelExists) {
+            throw new ApiError(
+                HttpStatusEnum.BAD_REQUEST,
+                `Model '${dto.model}' for make '${make}' already exists on our site!`,
+            );
+        }
+
         emailService
             .sendEmail(mainConfig.EMAIL_SUPPORT, EMAIL_DATA.CAR_MODEL_MISSING, {
-                model: dto.model,
+                model: model,
+                make: make,
             })
             .catch();
     }
