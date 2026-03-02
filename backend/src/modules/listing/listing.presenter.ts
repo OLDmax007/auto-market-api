d76066d8d329b5db9324a5f30523af978981ad8a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-02 23:46:36 +0200

    refactor(listing): update ListingPresenter

--- backend/src/modules/listing/listing.presenter.ts
+++ backend/src/modules/listing/listing.presenter.ts
@@ -1,62 +1,63 @@
-// import { PlatformRoleEnum } from "./enums/platform-role.enum";
-// import { ListingType } from "../types/listing.type";
-// import { PlatformRoleType } from "./types/platform-role.type";
-//
-// class ListingPresenter {
-//     // Базові поля для публічного перегляду (те, що бачать усі)
-//     private getPublicFields(listing: ListingType) {
-//         return {
-//             _id: listing._id,
-//             userId: listing.userId,
-//             organizationId: listing.organizationId,
-//             title: listing.title,
-//             description: listing.description,
-//             make: listing.make,
-//             model: listing.model,
-//             year: listing.year,
-//             mileage_km: listing.mileage_km,
-//             engineType: listing.engineType,
-//             transmission: listing.transmission,
-//             prices: listing.prices,
-//             country: listing.country,
-//             region: listing.region,
-//             city: listing.city,
-//             main_photo_url: listing.main_photo_url,
-//             publishedAt: listing.publishedAt,
-//         };
-//     }
-//
-//     public toResponse(
-//         listing: ListingType,
-//         role: PlatformRoleType,
-//     ): Partial<ListingType> {
-//         const publicFields = this.getPublicFields(listing);
-//
-//         switch (role) {
-//             case PlatformRoleEnum.ADMIN:
-//             case PlatformRoleEnum.MANAGER:
-//                 return {
-//                     ...publicFields,
-//                     isActive: listing.isActive,
-//                     profanityCheckAttempts: listing.profanityCheckAttempts,
-//                     createdAt: listing.createdAt,
-//                     updatedAt: listing.updatedAt,
-//                 };
-//
-//             default:
-//                 return publicFields;
-//         }
-//     }
-//
-//     public toPrivateResponse(listing: ListingType): Partial<ListingType> {
-//         return {
-//             ...this.getPublicFields(listing),
-//             isActive: listing.isActive,
-//             profanityCheckAttempts: listing.profanityCheckAttempts,
-//             createdAt: listing.createdAt,
-//             updatedAt: listing.updatedAt,
-//         };
-//     }
-// }
-//
-// export const listingPresenter = new ListingPresenter();
+import { mainConfig } from "../../common/configs/main.config";
+import { PlatformRoleEnum } from "../user/enums/platform-role.enum";
+import { ListingType } from "./types/listing.type";
+
+export class ListingPresenter {
+    private static getPublicFields(listing: ListingType): Partial<ListingType> {
+        return {
+            _id: listing._id,
+            userId: listing.userId,
+            organizationId: listing.organizationId,
+            title: listing.title,
+            description: listing.description,
+            make: listing.make,
+            model: listing.model,
+            year: listing.year,
+            mileage_km: listing.mileage_km,
+            prices: listing.prices,
+            country: listing.country,
+            region: listing.region,
+            city: listing.city,
+            poster: `${mainConfig.AWS_S3_BUCKET_URL}/${listing.poster}`,
+            publishedAt: listing.publishedAt,
+        };
+    }
+
+    public static toResponseByRole(
+        listing: ListingType,
+        role: PlatformRoleEnum,
+    ): Partial<ListingType> {
+        const publicFields = this.getPublicFields(listing);
+
+        switch (role) {
+            case PlatformRoleEnum.ADMIN:
+            case PlatformRoleEnum.MANAGER:
+                return {
+                    ...publicFields,
+                    isActive: listing.isActive,
+                    isProfanity: listing.isProfanity,
+                    profanityCheckAttempts: listing.profanityCheckAttempts,
+                    createdAt: listing.createdAt,
+                    updatedAt: listing.updatedAt,
+                };
+
+            default:
+                return publicFields;
+        }
+    }
+
+    public static toPrivateResponse(
+        listing: ListingType,
+    ): Partial<ListingType> {
+        return {
+            ...this.getPublicFields(listing),
+            isProfanity: listing.isProfanity,
+            isActive: listing.isActive,
+            createdAt: listing.createdAt,
+        };
+    }
+
+    public static toPublicResponse(listing: ListingType): Partial<ListingType> {
+        return this.getPublicFields(listing);
+    }
+}
