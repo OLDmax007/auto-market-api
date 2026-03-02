5059bb3cb7ccc4518d19f9eb14c09687b7dd1721
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-03 00:01:37 +0200

    refactor(constants): update endpoint in DEFAULT_IMAGES_ENDPOINTS

--- backend/src/common/constants/default-images-endpoints.constants.ts
+++ backend/src/common/constants/default-images-endpoints.constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_IMAGES_ENDPOINTS = {
-    user: `/default/avatar.png`,
-    listing: `/default/poster.jpg`,
+    user: `default/avatar.png`,
+    listing: `default/poster.jpg`,
 };
