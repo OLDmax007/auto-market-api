27973d74832ff5f897f5b0dcdae75422136be9e4
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-09 18:22:35 +0200

    feat(models): add sessionId field to Token model

--- backend/src/modules/auth/token.model.ts
+++ backend/src/modules/auth/token.model.ts
@@ -1,11 +1,19 @@
+import { randomUUID } from "node:crypto";
+
 import { model, Schema } from "mongoose";
 
 import { TokenType } from "./token.type";
 
 const tokenSchema = new Schema(
     {
-        accessToken: { type: String, unique: true, required: true },
-        refreshToken: { type: String, unique: true, required: true },
+        accessToken: { type: String, required: true },
+        refreshToken: { type: String, required: true },
+        sessionId: {
+            type: String,
+            unique: true,
+            required: true,
+            default: () => randomUUID(),
+        },
         userId: {
             type: Schema.Types.ObjectId,
             ref: "User",
