cc2822d0abc86c88c838ef1219c9a448ba77d3b8
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-09 23:36:45 +0200

    refactor(routes): add router.use for auth middlewares in subscriptionRouter

--- backend/src/modules/subscription/staff-subscription.router.ts
+++ backend/src/modules/subscription/staff-subscription.router.ts
@@ -7,29 +7,33 @@ import { PlatformPermissionEnum } from "../user/enums/platform-permission.enum";
 import { roleMiddleware } from "../user/middlewares/role.middleware";
 import { userMiddleware } from "../user/middlewares/user.middleware";
 import { subscriptionController } from "./subscription.controller";
+import { SubscriptionValidator } from "./subscription.validator";
 
 const router = Router();
+
+router.use(
+    authMiddleware.checkToken(TokenEnum.ACCESS),
+    userMiddleware.isActiveUser,
+    userMiddleware.isVerifiedUser,
+);
 
 router.patch(
     "/:userId",
     commonMiddleware.isValidId("userId"),
-    authMiddleware.checkToken(TokenEnum.ACCESS),
-    userMiddleware.isActiveUser,
-    userMiddleware.isVerifiedUser,
     roleMiddleware.checkPermission(
         PlatformPermissionEnum.SUBSCRIPTION_SET_PLAN,
+        "You do not have permission to change user subscription plans",
     ),
+    commonMiddleware.validateBody(SubscriptionValidator.setPlan),
     subscriptionController.setSubscriptionPlanByUserId,
 );
 
 router.patch(
     "/:userId/activate",
     commonMiddleware.isValidId("userId"),
-    authMiddleware.checkToken(TokenEnum.ACCESS),
-    userMiddleware.isActiveUser,
-    userMiddleware.isVerifiedUser,
     roleMiddleware.checkPermission(
         PlatformPermissionEnum.SUBSCRIPTION_ACTIVATE,
+        "You do not have permission to activate user subscriptions",
     ),
     subscriptionController.activate,
 );
@@ -37,13 +41,11 @@ router.patch(
 router.patch(
     "/:userId/deactivate",
     commonMiddleware.isValidId("userId"),
-    authMiddleware.checkToken(TokenEnum.ACCESS),
-    userMiddleware.isActiveUser,
-    userMiddleware.isVerifiedUser,
     roleMiddleware.checkPermission(
         PlatformPermissionEnum.SUBSCRIPTION_DEACTIVATE,
+        "You do not have permission to deactivate user subscriptions",
     ),
     subscriptionController.deactivate,
 );
 
-export const subscriptionRouter = router;
+export const staffSubscriptionRouter = router;
