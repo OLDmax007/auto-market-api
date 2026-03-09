68d54146ea52b5617e0c7e56f0d74a30cc133942
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-09 23:31:00 +0200

    feat(middlewares): add validateBody and validateParams to CommonMiddleware

--- backend/src/common/middlewares/common.middleware.ts
+++ backend/src/common/middlewares/common.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
 import { isObjectIdOrHexString } from "mongoose";
 
 import { HttpStatusEnum } from "../enums/http-status.enum";
@@ -8,16 +9,47 @@ class CommonMiddleware {
     public isValidId(key: string) {
         return (req: Request, res: Response, next: NextFunction) => {
             try {
-                const id = req.params[key];
-                if (!isObjectIdOrHexString(id)) {
-                    throw new ApiError(
-                        HttpStatusEnum.BAD_REQUEST,
-                        `Invalid ${key}`,
+                if (!isObjectIdOrHexString(req.params[key])) {
+                    return next(
+                        new ApiError(
+                            HttpStatusEnum.BAD_REQUEST,
+                            `Invalid ${key} format`,
+                        ),
                     );
                 }
                 next();
             } catch (e) {
                 next(e);
+            }
+        };
+    }
+
+    public validateBody(schema: ObjectSchema) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                req.body = await schema.validateAsync(req.body, {
+                    abortEarly: true,
+                });
+                next();
+            } catch (e) {
+                const message =
+                    e.details?.[0]?.message.replace(/"/g, "") || e.message;
+                next(new ApiError(HttpStatusEnum.BAD_REQUEST, message));
+            }
+        };
+    }
+
+    public validateParams(schema: ObjectSchema) {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                req.params = await schema.validateAsync(req.params, {
+                    abortEarly: true,
+                });
+                next();
+            } catch (e) {
+                const message =
+                    e.details?.[0]?.message.replace(/"/g, "") || e.message;
+                next(new ApiError(HttpStatusEnum.BAD_REQUEST, message));
             }
         };
     }
