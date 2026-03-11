65eaf6347fa6681a46237d8621917f88e3cdb931
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-11 17:32:19 +0200

    refactor(constants): update full patterns in REGEX_CONSTANTS

--- backend/src/common/constants/regex.constants.ts
+++ backend/src/common/constants/regex.constants.ts
@@ -1,13 +1,14 @@
 export const REGEX_CONSTANTS = {
     CAR: {
-        MODEL: /^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/,
+        MODEL: /^[a-zA-Z0-9]+(?:[ -][a-zA-Z0-9]+)*$/,
     },
     LISTING: {
-        TITLE: /^[a-zA-Zа-яА-Я0-9\s\-\!\?\.\(\)\,]+$/,
-        DESCRIPTION: /^(?!.*(http|https|www)).*$/i,
+        TITLE: /^[a-zA-Zа-яА-ЯіІїЇєЄ0-9 \-!?.,()"':/]+$/,
+        DESCRIPTION: /^(?!.*\b(?:https?:\/\/|www\.))/i,
     },
     USER: {
-        NAME: /^[A-ZА-ЯЁІЇЄ][a-zа-яёіїє']+(?:[-][A-ZА-ЯЁІЇЄ][a-zа-яёіїє']+)*$/,
-        PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+        NAME: /^[A-ZА-ЯІЇЄ][a-zа-яіїє']+(?:[-'][A-ZА-ЯІЇЄ][a-zа-яіїє']+)*$/,
+        PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+        EMAIL: /^[A-Za-z0-9._%+-]{1,64}@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)+$/,
     },
 };
