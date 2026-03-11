3d35e4a44e4567e939f935f6388853bc5e5a66f9
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-11 17:41:54 +0200

    feat(backend): add swagger documentation and middleware setup to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import "./index-models";
 
 import express, { NextFunction, Request, Response } from "express";
 import fileUpload from "express-fileupload";
+import swaggerUi from "swagger-ui-express";
 
 import { apiRouter } from "./api.router";
 import { mainConfig } from "./common/configs/main.config";
 import { ApiError } from "./common/errors/api.error";
 import { dataBaseService } from "./common/services/database.service";
+import { swaggerDocument } from "./docs/swagger/swagger";
 import { cronRunner } from "./index-crons";
 
 const app = express();
@@ -14,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", apiRouter);
 
 app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
@@ -24,11 +27,12 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
 
 const startServer = async () => {
     try {
-        const PORT = mainConfig.PORT;
+        const { PORT, BACKEND_URL } = mainConfig;
         await dataBaseService.connectToDB();
         app.listen(PORT, () => {
             cronRunner();
             console.log(`Server is starting on the port ${PORT}`);
+            console.log(`API docs: ${BACKEND_URL}/docs`);
         });
     } catch (e) {
         console.error(e);
