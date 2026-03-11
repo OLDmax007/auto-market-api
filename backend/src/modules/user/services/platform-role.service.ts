08a872d56b225f2a44d7a8cf861affb92cd71d84
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2026-03-11 21:17:28 +0200

    feat(roles): add caching logic to PlatformRoleService

--- backend/src/modules/user/services/platform-role.service.ts
+++ backend/src/modules/user/services/platform-role.service.ts
@@ -1,19 +1,27 @@
+import NodeCache from "node-cache";
+
 import { HttpStatusEnum } from "../../../common/enums/http-status.enum";
 import { ApiError } from "../../../common/errors/api.error";
 import { PlatformRoleEnum } from "../enums/platform-role.enum";
 import { platformRoleRepository } from "../repositories/platform-role.repository";
 import { PlatformRoleType } from "../types/platform-role.type";
 
+const roleCache = new NodeCache({ stdTTL: 3600, checkperiod: 600 });
+
 class PlatformRoleService {
     public async getPlatformRole(
         roleName: PlatformRoleEnum,
     ): Promise<PlatformRoleType> {
+        const cachedRole = roleCache.get<PlatformRoleType>(roleName);
+        if (cachedRole) return cachedRole;
+
         const role = await platformRoleRepository.getByName(roleName);
 
         if (!role) {
             throw new ApiError(HttpStatusEnum.NOT_FOUND, "Role not found");
         }
 
+        roleCache.set(roleName, role);
         return role;
     }
 
@@ -22,10 +30,16 @@ class PlatformRoleService {
     ): Promise<PlatformRoleType> {
         const role = await platformRoleRepository.getById(roleId);
 
+        const cacheKey = `id_${roleId}`;
+        const cachedRole = roleCache.get<PlatformRoleType>(cacheKey);
+
+        if (cachedRole) return cachedRole;
+
         if (!role) {
             throw new ApiError(HttpStatusEnum.NOT_FOUND, "Role not found");
         }
 
+        roleCache.set(cacheKey, role);
         return role;
     }
 }
